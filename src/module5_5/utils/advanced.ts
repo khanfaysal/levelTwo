65373bea126b9d20999ec7c5cd6bbcea3499fa29
khan faysal <mdfaysalkhancse@gmail.com>
2023-05-10 11:45:48 +0600

    create a model using multiple interface for class

--- src/module5_5/utils/advanced.ts
+++ src/module5_5/utils/advanced.ts
@@ -1,6 +1,9 @@
 // properties
 
-interface 
+interface Model<TData, TMethods>{
+    data: TData;
+    methods: TMethods;
+}
 
 interface IUser {
     firstName: string;
@@ -13,4 +16,21 @@ interface IMethods {
     fullName(): string;
 }
 
-type model
+type model = Model<IUser, IMethods>
+
+class User implements model {
+    data: IUser;
+    methods: IMethods
+
+    constructor(firstName: string, lastName:string) {
+        this.data = {firstName, lastName}
+        this.methods = {
+            fullName: () => `${firstName} ${lastName}`
+        }
+    }
+}
+
+const user1 = new User('faysal', 'khan');
+
+console.log(user1.methods.fullName())
+// console.log('user1', user1)
