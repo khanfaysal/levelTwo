a16da0f7181328c8ff1a473837dd7893c934c006
khan faysal <mdfaysalkhancse@gmail.com>
2023-05-10 08:08:21 +0600

    typescript global naming conflict test and solved

---
+++ src/module5_5/main.ts
@@ -0,0 +1,6 @@
+import {addNumberTest as addTwoSum} from './module'
+
+const addNumberTest = (num1: number,num2: number, num3: number): number => {
+    return num1 + num2 + num3;
+}
+const res = addTwoSum(4,6)
---
+++ src/module5_5/module.ts
@@ -0,0 +1,3 @@
+export const addNumberTest = (num1: number, num2:number): number => {
+    return num1 + num2;
+}
